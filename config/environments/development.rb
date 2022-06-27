479a34bff839b6730340ec9609768e47822641c4
Efremov Evgenii <e.efremov@3commas.io>
2022-06-27 20:05:38 +0300

    add nginx conf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ end
 
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = false
+  config.webpacker.check_yarn_integrity = yarn_integrity_enabled?
 
   # Settings specified here will take precedence over those in config/application.rb.
 
