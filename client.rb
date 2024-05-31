eeed4dd0d84b6ff6482a3d50dee6387409e52379
Alexey Vasilyev <bestspajic@gmail.com>
2024-06-01 02:38:30 +0300

    Clean up

--- client.rb
+++ client.rb
@@ -75,4 +75,4 @@ c123 = collect_sorted([c1, c2, c3])
 result = a(c123)
 
 puts "FINISHED in #{Time.now - start}s."
-puts "RESULT = #{result}"
+puts "RESULT = #{result}" # 0bbe9ecf251ef4131dd43e1600742cfb
--- ping.rb
+++ ping.rb
@@ -4,6 +4,7 @@ require 'async'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+# ruby ping.rb a 3
 ARGV[1].to_i.times.map do
   Thread.new do
     puts "https://localhost:9292/#{ARGV[0]}?value=ping"
