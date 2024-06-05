39ae4811c0f7d31dfb56c6a6b03e3bb8891fceb1
Nikita Kononov <vocrsz@gmail.com>
2024-06-05 21:45:53 +0200

    purify first solution

--- client.rb
+++ client.rb
@@ -23,24 +23,18 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #   - одновременно можно запускать не более одного
 #
 def a(value)
-  ->() {
-    puts "https://localhost:9292/a?value=#{value}"
-    Async { Faraday.get("https://localhost:9292/a?value=#{value}").body }
-  }
+  puts "https://localhost:9292/a?value=#{value}"
+  Async { Faraday.get("https://localhost:9292/a?value=#{value}").body }
 end
 
 def b(value)
-  ->() {
-    puts "https://localhost:9292/b?value=#{value}"
-    Async { Faraday.get("https://localhost:9292/b?value=#{value}").body }
-  }
+  puts "https://localhost:9292/b?value=#{value}"
+  Async { Faraday.get("https://localhost:9292/b?value=#{value}").body }
 end
 
 def c(value)
-  ->() {
-    puts "https://localhost:9292/c?value=#{value}"
-    Async { Faraday.get("https://localhost:9292/c?value=#{value}").body }
-  }
+  puts "https://localhost:9292/c?value=#{value}"
+  Async { Faraday.get("https://localhost:9292/c?value=#{value}").body }
 end
 
 # Референсное решение, приведённое ниже работает правильно, занимает ~19.5 секунд
@@ -52,81 +46,74 @@ end
 
 start = Time.now
 
-a11, a12, a13, a21, a22, a23, a31, a32, a33,
+a11, a12, a13,
+a21, a22, a23,
+a31, a32, a33,
 b1, b2, b3,
 ab1, ab2, ab3,
 c1, c2, c3,
 result = nil
 
-a1_barrier = Async::Barrier.new
-a2_barrier = Async::Barrier.new
-a3_barrier = Async::Barrier.new
-b1_barrier = Async::Barrier.new
-b3_barrier = Async::Barrier.new
-c1_barrier = Async::Barrier.new
-c2_barrier = Async::Barrier.new
-c3_barrier = Async::Barrier.new
-
-# a_semaphore = Async::Semaphore.new(3)
-# b_semaphore = Async::Semaphore.new(2)
+a_barrier = Async::Barrier.new
+b_barrier = Async::Barrier.new
+c_barrier = Async::Barrier.new
 
 Async do
   Async do
     a11, a12, a13 = [a(11), a(12), a(13)].map do |task|
-      a1_barrier.async do
-        task.call.wait
-        # a11, a12, a13 = [a(11).call.wait, a(12).call.wait, a(13).call.wait]
+      a_barrier.async do
+        task.wait
       end
     end
 
     b1, b2 = [b(1), b(2)].map do |task|
-      b1_barrier.async do
-        task.call.wait
+      b_barrier.async do
+        task.wait
       end
     end
 
-    a1_barrier.wait
+    a_barrier.wait
     a21, a22, a23 = [a(21), a(22), a(23)].map do |task|
-      a2_barrier.async do
-        task.call.wait
+      a_barrier.async do
+        task.wait
       end
     end
 
-    b1_barrier.wait
-    b3_barrier.async do
-      b3 = b(3).call.wait
+    b_barrier.wait
+    b_barrier.async do
+      b3 = b(3).wait
     end
     ab1 = "#{collect_sorted([a11.wait, a12.wait, a13.wait])}-#{b1.wait}"
 
-    c1_barrier.async do
-      c1 = c(ab1).call.wait
+    c_barrier.async do
+      c1 = c(ab1).wait
     end
 
-    a2_barrier.wait
+    a_barrier.wait
     a31, a32, a33 = [a(31), a(32), a(33)].map do |task|
-      a3_barrier.async do
-        task.call.wait
+      a_barrier.async do
+        task.wait
       end
     end
     ab2 = "#{collect_sorted([a21.wait, a22.wait, a23.wait])}-#{b2.wait}"
 
-    c1_barrier.wait
-    c2_barrier.async do
-      c2 = c(ab2).call.wait
+    c_barrier.wait
+    c_barrier.async do
+      c2 = c(ab2).wait
     end
 
-    a3_barrier.wait
-    b3_barrier.wait
+    a_barrier.wait
+    b_barrier.wait
     ab3 = "#{collect_sorted([a31.wait, a32.wait, a33.wait])}-#{b3}"
 
-    c2_barrier.wait
-    c3_barrier.async do
-      c3 = c(ab3).call.wait
+    c_barrier.wait
+    c_barrier.async do
+      c3 = c(ab3).wait
     end
 
-    c3_barrier.wait
+    c_barrier.wait
     c123 = collect_sorted([c1, c2, c3])
-    result = a(c123).call.wait
+    result = a(c123).wait
   end
 end
 
